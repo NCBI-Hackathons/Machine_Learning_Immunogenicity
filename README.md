fcd8f1dc6fe9f8b80aa9fcf02cbf08fc7e4e2cf2
Ubuntu <ubuntu@ip-172-16-240-184.ec2.internal>
2016-08-17 20:38:51 +0000

    Added details about src/run-data-on-predictor.py

--- README.md
+++ README.md
@@ -11,3 +11,59 @@ Initial application of Logistic Regression (LR) and Neural Networks (NN) looks p
 The following are provided:
 * R scripts for data wrangling of IEDB data and mashup with Amino Acid properties
 * Python Notebook for application of Logistic Regression and Neural Networks using Tensorflow
+* Python script for computing binding affinities using several published approaches based on several datasets
+
+## Run predictors on published data
+
+This script allows to run published epitope binding predictors on immunogenicity data from IEDB T-cell and MHC assays (http://www.iedb.org/), as well as data from IMMA2. This script can be used to test whether binding predictions also predict immugenicity (Note: they don't). We use the data interface implemented in the pepdata package from the Hammer Lab (https://github.com/hammerlab/pepdata) and the implementations of various predictors in the Fred 2 framework for computational immunogenomics by Schubert et al. (https://github.com/FRED-2/Fred2 and http://bioinformatics.oxfordjournals.org/content/32/13/2044).
+
+### Usage
+
+    usage: run-data-on-predictor.py [-h] --predictor PREDICTOR --dataset DATASET
+                                    [-n N] [--allele ALLELE]
+    
+    Call epitope predictors on data.
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -n N                  Number of rows to take from dataset
+      --allele ALLELE       Allelle
+    
+    required arguments:
+      --predictor PREDICTOR
+                            Epitope predictors [see all with --predictor=list]
+      --dataset DATASET     Immunogenic dataset [see all with --dataset=list]
+
+
+## List available predictors and datasets
+
+    run-data-on-predictor.py  --predictor list --dataset list
+
+Set one of the predictors with --predictor:
+['smmpmbec', 'syfpeithi', 'netctlpan', 'smm', 'tepitopepan', 'netmhcii', 'arb', 'pickpocket', 'epidemix', 'unitope', 'netmhciipan', 'comblibsidney', 'netmhcpan', 'calisimm', 'hammer', 'svmhc', 'bimas']
+
+Details from https://bioinformatics.oxfordjournals.org/content/suppl/2016/02/26/btw113.DC1/S1.pdf
+
+| SYFPEITHI | T-cell epitope | (Rammensee, et al., 1999)|
+| BIMAS | MHC-I binding | (Parker, et al., 1994)|
+| SVMHC | MHC-I binding | (Dönnes and Elofsson, 2002)|
+| ARB | MHC-I binding | (Bui, et al., 2005)|
+| SMM | MHC-I binding | (Peters and Sette, 2005)|
+| SMMPMBEC | MHC-I binding | (Kim, et al., 2009)|
+| Epidemix | MHC-I binding | (Feldhahn, et al., 2009)|
+| Comblib | MHC-I binding | (Sidney, et al., 2008)|
+| PickPocket* | MHC-I binding | (Zhang, et al., 2009)|
+| NetMHC* | MHC-I binding | (Lundegaard, et al., 2008)|
+| NetMHCpan* | MHC-I binding | (Hoof, et al., 2009)|
+| HAMMER | MHC-II binding | (Sturniolo, et al., 1999)|
+| TEPITOPEpan | MHC-II binding | (Zhang, et al., 2012)|
+| NetMHCII* | MHC-II binding | (Nielsen, et al., 2007)|
+| NetMHCIIpan* | MHC-II binding | (Karosiene, et al., 2013)|
+| UniTope | T-cell epitope | (Toussaint, et al., 2011)|
+| NetCTLpan* | T-cell epitope | (Stranzl, et al., 2010)|
+
+available datasets: iedb.tcell, ideb.mhc, imma2
+
+## Example output
+
+
--- src/run-data-on-predictor.py
+++ src/run-data-on-predictor.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Author: F P Breitwieser
+
 from __future__ import print_function, division, absolute_import
 import numpy as np
 import argparse
-from sys import exit
+import sys
+from sys import exit, stdin
 
 from Fred2.Core import Allele, Peptide, Protein,generate_peptides_from_proteins
 from Fred2.IO import read_lines, read_fasta
 from Fred2.EpitopePrediction import EpitopePredictorFactory
 #from nose.tools import eq_
 
-from pepdata import iedb
+import pepdata
+import pandas as pd
+pd.set_option('display.height', 1000)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 200)
+pd.set_option('display.width', 200)
+
 
 parser = argparse.ArgumentParser(description='Call epitope predictors on data.')
-parser.add_argument('predictor', type=str, help='Epitope predictors [see all with --predictor=list]')
-parser.add_argument('dataset', type=str, help='Immunogenic dataset [see all with --dataset=list]')
+requiredNamed = parser.add_argument_group('required arguments')
+requiredNamed.add_argument('--predictor', type=str, help='Epitope predictors [see all with --predictor=list]', required=True)
+requiredNamed.add_argument('--dataset', type=str, help='Immunogenic dataset [see all with --dataset=list]', required=True)
 parser.add_argument('-n', type=int, help='Number of rows to take from dataset')
+parser.add_argument('--allele', type=str, help='HLA Type', default=["HLA-A*01:01","HLA-A*02:01","HLA-B*15:01"])
 
 args = parser.parse_args()
 
+all_predictors = [ name for name,version in EpitopePredictorFactory.available_methods().iteritems()]
+
+all_predictors.remove("netmhcstabpan")
+all_predictors.remove("netmhc")
 
 if args.predictor == 'list':
 	print("Set one of the predictors with --predictor:")
-	print([ name for name,version in EpitopePredictorFactory.available_methods().iteritems()].sort())
+	print(all_predictors)
 	print ("""
 Details from https://bioinformatics.oxfordjournals.org/content/suppl/2016/02/26/btw113.DC1/S1.pdf
  SYFPEITHI     T-cell epitope  (Rammensee, et al., 1999)
@@ -47,27 +62,72 @@ Details from https://bioinformatics.oxfordjournals.org/content/suppl/2016/02/26/
 
 _imm, _non = set(), set()
 if args.dataset == 'iedb.tcell':
-	_imm, _non = iedb.tcell.load_classes(nrows=args.n)
-elif args.dataset == 'iedb.bcell':
-	_imm, _non = iedb.bcell.load_classes(nrows=args.n)
+	_imm, _non = pepdata.iedb.tcell.load_classes(nrows=args.n)
 elif args.dataset == 'iedb.mhc':
-	_imm, _non = iedb.mhc.load_classes(nrows=args.n)
+	_imm, _non = pepdata.iedb.mhc.load_classes(nrows=args.n)
+elif args.dataset == 'imma2':
+	_imm, _non = pepdata.imma2.load_classes()
+elif args.dataset == 'stdin':
+	_imm = [ line for line in stdin ]
 else:
-	print("available datasets: iedb.tcell, ideb.mhc")
+	print("available datasets: iedb.tcell, ideb.mhc, imma2")
 	exit()
 
 imm = [Peptide(elem) for elem in _imm]
 non = [Peptide(elem) for elem in _non]
 
-predictor = EpitopePredictorFactory(args.predictor)
-results = predictor.predict(imm)
-print("POSITIVES:")
-print(results.head())
+#hla='HLA-A2|HLA-A\*02'
+#df = ()
+#
+#if args.dataset == 'iedb.tcell':
+#	df =  pepdata.iedb.tcell.load_dataframe(nrows=args.n)
+#elif args.dataset == 'iedb.mhc':
+#	df =  pepdata.iedb.mhc.load_dataframe(nrows=args.n)
+#elif args.dataset == 'iedb.bcell':
+#	df =  pepdata.iedb.bcell.load_dataframe(nrows=args.n)
+#elif args.dataset == 'stdin':
+#	df = [ line for line in stdin ]
+#else:
+#	print("available datasets: iedb.tcell, ideb.mhc, imma2")
+#	exit()
+#
+#print(df.head())
+#
 
-print("NEGATIVES:")
-results = predictor.predict(non)
-print(results.head())
+def run_predictor(pred, dataset):
+	predictor = EpitopePredictorFactory(pred)
+	results = ()
+	try:
+		results = predictor.predict(dataset, alleles=[ Allele(a) for a in args.allele ])
+		print(results)
+		print(results.describe())
+	except ValueError:
+		pass
+	
+	return(len(results),len(dataset))
+	
+def print_res(pred, res, dtype):
+	print("RESULTS for %20s / %s: got scores for %5d/%d positives and %5d/%d negatives." % ((pred,dtype,) + res))
 
+if args.predictor == 'all':
+	for pred in all_predictors:
+		try:
+			print (pred," immunogenic peptides")
+			print_res(pred, run_predictor(pred, imm), "    immunogenic peptides")
+			if len(non) > 0:		
+				print (pred," non-immunogenic peptides")
+				print_res(pred, run_predictor(pred, non), "non-immunogenic peptides")
+		except:
+			pass
+else:
+	print (pred," immunogenic peptides")
+	print_res(args.predictor,run_predictor(args.predictor, imm))
+	if len(non) > 0:		
+		print (pred," non-immunogenic peptides")
+		print_res(args.predictor,run_predictor(args.predictor, non))
+
+
+#print(results_imm)
 
 
 #tcell_df = iedb.tcell.load_dataframe(nrows=1000)
